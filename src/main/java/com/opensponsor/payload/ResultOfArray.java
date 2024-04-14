6ab1485295ce4b0d63616ce2868ecf106e119bdd
foxsir <contact@umbst.com>
2024-04-14 20:15:33 +0800

    update ResultOfArray

--- src/main/java/com/opensponsor/payload/ResultOfArray.java
+++ src/main/java/com/opensponsor/payload/ResultOfArray.java
@@ -2,16 +2,57 @@ package com.opensponsor.payload;
 
 import java.util.List;
 
-public class ResultOfArray<E> {
+public class ResultOfArray {
 
-    int currentPageNumber;
-    int lastPageNumber;
-    int pageSize;
-    long totalRecords;
-    String message;
-    List<E> records;
+    public int currentPageNumber;
+    public int lastPageNumber;
+    public int pageSize;
+    public long totalRecords;
+    public String message;
+    public List<Object> records;
+    public int code = 0;
 
-    public ResultOfArray(List<E> records) {
-        this.records = records;
+
+    public static class Builder {
+        public ResultOfArray build() {
+            return new ResultOfArray(this);
+        }
+
+        public Builder data(List<Object> records) {
+            this.records = records;
+            return this;
+        }
+
+        public Builder code(int code) {
+            this.code = code;
+            return this;
+        }
+
+        public Builder message(String message) {
+            this.message = message;
+            return this;
+        }
+
+        public int currentPageNumber;
+        public int lastPageNumber;
+        public int pageSize;
+        public long totalRecords;
+        public String message;
+        public List<Object> records;
+        public int code = 0;
+    }
+
+    public static ResultOfArray.Builder builder() {
+        return new ResultOfArray.Builder();
+    }
+
+    private ResultOfArray(Builder builder) {
+        currentPageNumber = builder.currentPageNumber;
+        lastPageNumber = builder.lastPageNumber;
+        pageSize = builder.pageSize;
+        totalRecords = builder.totalRecords;
+        message = builder.message;
+        records = builder.records;
+        code = builder.code;
     }
 }
