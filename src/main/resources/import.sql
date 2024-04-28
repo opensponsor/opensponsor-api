182578c57466dcbf4e8780c369d550457a55c54c
foxsir <contact@umbst.com>
2024-04-28 20:59:11 +0800

    initialize data

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,6 +1,5 @@
 -- This file allow to write SQL commands that will be emitted in test and dev.
 -- The commands are commented as their support depends of the database
--- insert into myentity (id, field) values(1, 'field-1');
--- insert into myentity (id, field) values(2, 'field-2');
--- insert into myentity (id, field) values(3, 'field-3');
--- alter sequence myentity_seq restart with 4;
+
+-- initialize country codes
+INSERT INTO public.country_codes (when_created, when_deleted, when_modified, country_codes, currency_alphabetic_code, dial, id, languages, cldr_display_name, official_name_cn, official_name_en) VALUES ('2024-04-27 09:37:55.346630 +00:00', null, '2024-04-27 09:37:55.346672 +00:00', 'CN', 'CNY', '86', '0466037a-b655-4629-a748-35ca3789d68f', 'zh-CN', 'China', '中华人民共和国', 'China');
