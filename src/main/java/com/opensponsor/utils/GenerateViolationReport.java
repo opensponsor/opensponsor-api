959324138022e7dee57e982436227931c6225aee
foxsir <contact@umbst.com>
2024-05-01 12:42:45 +0800

    use setParameterViolations

--- src/main/java/com/opensponsor/utils/GenerateViolationReport.java
+++ src/main/java/com/opensponsor/utils/GenerateViolationReport.java
@@ -1,21 +1,30 @@
 package com.opensponsor.utils;
 
+import jakarta.enterprise.context.ApplicationScoped;
 import org.jboss.resteasy.api.validation.ResteasyConstraintViolation;
 import org.jboss.resteasy.api.validation.ViolationReport;
 
 import java.util.ArrayList;
 
+@ApplicationScoped
 public class GenerateViolationReport {
     private final ArrayList<ResteasyConstraintViolation> violations = new ArrayList<>();
+    private String exception;
 
     public GenerateViolationReport add(ResteasyConstraintViolation resteasyConstraintViolation) {
         violations.add(resteasyConstraintViolation);
         return this;
     }
 
+    public GenerateViolationReport exception(String exception) {
+        this.exception = exception;
+        return this;
+    }
+
     public ViolationReport build() {
         ViolationReport violationReport = new ViolationReport();
-        violationReport.setPropertyViolations(this.violations);
+        violationReport.setParameterViolations(this.violations);
+        violationReport.setException(this.exception);
 
         return violationReport;
     }
