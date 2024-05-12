c030ff9ece8f9175808bccce1ca2ec90b8ab99ce
foxsir <contact@umbst.com>
2024-05-12 22:15:08 +0800

    add get authUser method

--- src/main/java/com/opensponsor/repositorys/UserRepository.java
+++ src/main/java/com/opensponsor/repositorys/UserRepository.java
@@ -6,12 +6,18 @@ import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
 import io.quarkus.panache.common.Page;
 import io.quarkus.panache.common.Sort;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.core.SecurityContext;
 
 import java.util.List;
 import java.util.UUID;
 
 @ApplicationScoped
 public class UserRepository implements PanacheRepositoryBase<User, UUID> {
+    @Inject
+    SecurityContext ctx;
+
+
     public List<User> list(Page page, Sort sort) {
         return this.findAll(sort)
             .page(page)
@@ -20,5 +26,13 @@ public class UserRepository implements PanacheRepositoryBase<User, UUID> {
 
     public User findByName(String name) {
         return this.find("name", name).firstResult();
+    }
+
+    public User authUser() {
+        User user = null;
+        if(ctx.getUserPrincipal() != null) {
+            user = User.find("username", ctx.getUserPrincipal().getName()).firstResult();
+        }
+        return user;
     }
 }
