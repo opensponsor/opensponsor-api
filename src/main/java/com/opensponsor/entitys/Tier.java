2bfd77440155b86d586e72ea2c1dcbd7d915c5a5
foxsir <contact@umbst.com>
2024-05-25 17:03:31 +0800

    update organization and openapi @Schema

--- src/main/java/com/opensponsor/entitys/Tier.java
+++ src/main/java/com/opensponsor/entitys/Tier.java
@@ -1,12 +1,16 @@
 package com.opensponsor.entitys;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.opensponsor.enums.E_AMOUNT_TYPE;
 import com.opensponsor.enums.E_IBAN_CURRENCIES;
 import com.opensponsor.enums.E_INTERVAL;
 import com.opensponsor.enums.E_TIER_TYPE;
 import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.Size;
+import org.eclipse.microprofile.openapi.annotations.enums.SchemaType;
 import org.eclipse.microprofile.openapi.annotations.media.Schema;
 import org.hibernate.annotations.Comment;
 import org.hibernate.annotations.CreationTimestamp;
@@ -25,20 +29,25 @@ public class Tier extends PanacheEntityBase {
     public UUID id;
 
     // public declare CollectiveId: number;
-    @Schema(description = "所属组织")
+    @Schema(description = "所属组织", hidden = true)
     @ManyToOne(fetch = FetchType.EAGER, optional = false)
+    @JsonIgnore
     public Organization organization;
+
+    @Schema(description = "所属组织ID")
+    @Transient
+    public UUID organizationId;
 
     // public declare slug: string;
     @Comment("url slug")
-    @Schema(description = "url path")
-    @Column(length = 32, nullable = false)
+    @Schema(description = "url path", minLength = 2, maxLength = 32)
+    @Column(length = 32)
     @Size(min = 2, max = 32)
     public String slug;
 
     // public declare name: string;
     @Comment("tier name")
-    @Schema(description = "捐助等级名称")
+    @Schema(description = "捐助等级名称", minLength = 2, maxLength = 32, required = true)
     @Column(length = 32, nullable = false)
     @Size(min = 2, max = 32)
     public String name;
@@ -52,20 +61,20 @@ public class Tier extends PanacheEntityBase {
 
     // public declare description: string;
     @Comment("description")
-    @Schema(description = "描述")
+    @Schema(description = "描述", maxLength = 500, required = true)
     @Column(length = 500, nullable = false, columnDefinition = "TEXT")
     @Size(max = 500)
     public String description;
 
     // public declare longDescription: string;
     @Comment("longDescription")
-    @Schema(description = "捐助等级类型")
+    @Schema(description = "捐助等级类型", maxLength = 1000, required = false)
     @Column(columnDefinition = "TEXT")
     public String longDescription;
 
     // public declare useStandalonePage: boolean;
     @Comment("enable Standalone Page")
-    @Schema(description = "是否使用独立页面")
+    @Schema(description = "是否使用独立页面", required = true)
     @Column()
     public boolean useStandalonePage;
 
@@ -86,14 +95,15 @@ public class Tier extends PanacheEntityBase {
     @Comment("amount")
     @Schema(description = "捐助金额")
     @Column(length = 10, nullable = false)
-    @Size(min = 1, max = 90000)
+    @Min(1)
+    @Max(100000)
     public Number amount;
 
     // public declare presets: number[];
     @Comment("presets")
     @Schema(description = "捐助金额范围")
     @Column()
-    @ElementCollection
+    @ElementCollection(fetch = FetchType.EAGER)
     public List<Number> presets = List.of();
 
     // public declare amountType: 'FIXED' | 'FLEXIBLE';
@@ -107,7 +117,8 @@ public class Tier extends PanacheEntityBase {
     @Comment("minimumAmount")
     @Schema(description = "最小金额")
     @Column(length = 10)
-    @Size(min = 1, max = 90000)
+    @Min(1)
+    @Max(100000)
     public Number minimumAmount;
 
     // public declare currency: SupportedCurrency;
@@ -128,13 +139,15 @@ public class Tier extends PanacheEntityBase {
     @Comment("max quantity")
     @Schema(description = "库存")
     @Column(length = 10)
+    @Min(1)
+    @Max(100000)
     public Number maxQuantity;
 
     // public declare goal: number;
     @Comment("goal")
     @Schema(description = "筹款目标")
     @Column(length = 10)
-    public Number goal;
+    public Long goal;
 
     @CreationTimestamp
     public Instant whenCreated;
