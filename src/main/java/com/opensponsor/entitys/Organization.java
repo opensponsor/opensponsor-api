6944cc44c67249752b6525dfd92cdf0a36ce3e98
foxsir <contact@umbst.com>
2024-05-25 21:53:11 +0800

    update openapi Schema

--- src/main/java/com/opensponsor/entitys/Organization.java
+++ src/main/java/com/opensponsor/entitys/Organization.java
@@ -19,58 +19,69 @@ public class Organization extends PanacheEntityBase {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     @Column(unique = true, nullable = false)
+    @Schema(required = true)
     public UUID id;
 
-    @Comment("user name")
+    @Comment("Organization name")
     @Column(unique = true, length = 32, nullable = false)
     @Size(min = 2, max = 32)
+    @Schema(description = "Organization name", required = true, minLength = 2, maxLength = 32)
     public String name;
 
     @Comment("legal name")
     @Column(length = 32)
-    @Size(max = 32)
+    @Size(min = 2, max = 32)
+    @Schema(description = "Organization legal name", minLength = 2, maxLength = 32)
     public String legalName;
 
     @Comment("url slug")
     @Column(length = 32, nullable = false)
     @Size(min = 2, max = 32)
+    @Schema(description = "url slug", required = true, minLength = 2, maxLength = 32)
     public String slug;
 
     @Comment("introduce")
     @Column(length = 150)
     @Size(min = 2, max = 150)
+    @Schema(description = "introduce", minLength = 2, maxLength = 150)
     public String introduce;
 
     @Comment("organization types")
     @Enumerated
     @Column()
+    @Schema(description = "organization types", required = true, minLength = 2, maxLength = 150)
     public E_ORGANIZATION_TYPE type;
 
     @Comment("tags")
     @Column()
     @ElementCollection(fetch = FetchType.EAGER)
+    @Schema(description = "organization tags", required = true)
     public Set<String> tags = new HashSet<>();
 
     @Comment("website")
     @Column(length = 32)
     @Size(min = 2, max = 32)
+    @Schema(description = "website url", minLength = 2, maxLength = 150)
     public String website;
 
-    @Schema(description = "捐助等级")
     @OneToMany(fetch = FetchType.EAGER, mappedBy = "organization")
+    @Schema(description = "捐助等级", required = true)
     public List<Tier> tiers = new ArrayList<>();
 
-    @Schema(description = "Organization Owner")
     @ManyToOne(fetch = FetchType.EAGER, optional = false)
+    @Schema(description = "所属用户", required = true)
     public User user;
 
     @CreationTimestamp
+    @Schema(description = "when created", required = true)
     public Instant whenCreated;
 
     @UpdateTimestamp
+    @Schema(description = "when modified", required = true)
     public Instant whenModified;
 
     @SoftDelete
     @Column(nullable = true)
+    @Schema(description = "when deleted")
     public Instant whenDeleted;
 }
