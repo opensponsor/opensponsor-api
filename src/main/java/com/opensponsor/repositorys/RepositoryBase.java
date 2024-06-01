76d245f3f6ad47c22a86c7905d6b1bde2aa05bb2
foxsir <contact@umbst.com>
2024-06-01 21:39:55 +0800

    add RepositoryBase

---
+++ src/main/java/com/opensponsor/repositorys/RepositoryBase.java
@@ -0,0 +1,45 @@
+package com.opensponsor.repositorys;
+
+import com.opensponsor.entitys.Organization;
+import com.opensponsor.entitys.Tier;
+import com.opensponsor.entitys.User;
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
+import jakarta.inject.Inject;
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
+
+import java.util.UUID;
+
+public abstract class RepositoryBase<T extends PanacheEntityBase> implements PanacheRepositoryBase<T, UUID> {
+
+    @Inject
+    protected UserRepository userRepository;
+
+    @Transactional
+    public T create(T entity) {
+        entity.persistAndFlush();
+        return entity;
+    }
+
+    @Transactional
+    public T save(T entity) {
+        EntityManager em = this.getEntityManager();
+
+        if(entity.isPersistent()) {
+            entity.persist();
+        } else {
+            em.merge(entity);
+        }
+        return entity;
+    }
+
+    public boolean checkOwnership(User owner) {
+        User user = userRepository.authUser();
+        if(owner != null) {
+            return user.id.equals(owner.id);
+        }
+        return false;
+    }
+
+}
