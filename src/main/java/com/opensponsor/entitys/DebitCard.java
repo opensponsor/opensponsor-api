ccd4d2e8b7538a03aeb6cbd5c9ae711c30bd41b4
foxsir <contact@umbst.com>
2024-06-08 19:45:07 +0800

    add DebitCard

---
+++ src/main/java/com/opensponsor/entitys/DebitCard.java
@@ -0,0 +1,65 @@
+package com.opensponsor.entitys;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.opensponsor.utils.CDIGetter;
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.SoftDelete;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Entity
+public class DebitCard extends PanacheEntityBase {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(unique = true, nullable = false)
+    public UUID id;
+
+    @Column(length = 22)
+    @Size(min = 16, max = 22)
+    @Schema(description = "Debit Card No")
+    public String cardNo;
+
+    @Column(length = 16)
+    @Size(min = 2, max = 16)
+    @Schema(description = "legal name")
+    public String legalName;
+
+    @Column(length = 64)
+    @Size(min = 2, max = 64)
+    @Schema(description = "bank name")
+    public String bankName;
+
+    @OneToOne(optional = false)
+    @Schema(description = "create for organization")
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    public Organization organization;
+
+    @ManyToOne
+    @Schema(description = "create by user")
+    @JsonIgnore
+    public User user;
+
+    @CreationTimestamp
+    public Instant whenCreated;
+
+    @UpdateTimestamp
+    public Instant whenModified;
+
+    @SoftDelete
+    @Column(nullable = true)
+    public Instant whenDeleted;
+
+    @PrePersist
+    protected void prePersist() {
+        if(this.user == null) {
+            this.user = CDIGetter.getUserRepository().authUser();
+        }
+    }
+}
