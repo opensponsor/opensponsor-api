4b1406b28fbcd42d2b1edfdbf6df27f2293850d3
foxsir <838394225@qq.com>
2025-01-05 14:11:47 +0800

    update E_ORGANIZATION_TYPE

--- src/main/java/com/opensponsor/enums/E_ORGANIZATION_TYPE.java
+++ src/main/java/com/opensponsor/enums/E_ORGANIZATION_TYPE.java
@@ -13,7 +13,51 @@ public enum E_ORGANIZATION_TYPE {
      * common organization
      */
     @Schema(description = "通用社区")
-    COMMUNITY("COMMUNITY");
+    COMMUNITY("COMMUNITY"),
+
+    /**
+     * event
+     */
+    @Schema(description = "事件")
+    EVENT("EVENT"),
+
+    /**
+     * organization
+     */
+    @Schema(description = "组织")
+    ORGANIZATION("ORGANIZATION"),
+
+    /**
+     * bot
+     */
+    @Schema(description = "机器人")
+    BOT("BOT"),
+
+    /**
+     * project
+     */
+    @Schema(description = "项目")
+    PROJECT("PROJECT"),
+
+    /**
+     * fund
+     */
+    @Schema(description = "基金")
+    FUND("FUND"),
+
+    /**
+     * vendor
+     */
+    @Schema(description = "供应商")
+    VENDOR("VENDOR"),
+
+    /**
+     * user
+     */
+    @Schema(description = "用户")
+    USER("USER");
+
+
 
     public final String label;
 
