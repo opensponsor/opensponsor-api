eb4e187f8d037c29a1378f36e60979c20ba31dfa
foxsir <838394225@qq.com>
2025-01-07 20:48:06 +0800

    Revert "get github repository"
    This reverts commit 353e6947

--- src/main/java/com/opensponsor/payload/github/GithubLicense.java
+++
@@ -1,9 +0,0 @@
-package com.opensponsor.payload.github;
-
-public class GithubLicense {
-    public String key;
-    public String name;
-    public String spdx_id;
-    public String url;
-    public String node_id;
-}
