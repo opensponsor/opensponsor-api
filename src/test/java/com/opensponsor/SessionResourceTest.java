95d39753eb7ec1d1f766a2e2b6774321cd8c870c
foxsir <838394225@qq.com>
2025-02-01 18:54:20 +0800

    feat: add test

---
+++ src/test/java/com/opensponsor/SessionResourceTest.java
@@ -0,0 +1,35 @@
+package com.opensponsor;
+
+import com.opensponsor.entitys.CountryCodes;
+import com.opensponsor.enums.E_SEX;
+import com.opensponsor.payload.RegisterBody;
+import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.anything;
+import static org.hamcrest.CoreMatchers.is;
+
+@QuarkusTest
+class SessionResourceTest {
+    @Test
+    void testRegisterUser() {
+        RegisterBody body = new RegisterBody();
+        body.username = "霍建营";
+        body.slug = "huo-jian-ying";
+        body.countryCode = CountryCodes.findById(UUID.fromString("0466037a-b655-4629-a748-35ca3789d68f"));
+        body.sex = E_SEX.MAN;
+        body.password = "12345678";
+
+        given()
+            .when().contentType(ContentType.JSON)
+            .when().body(body)
+            .when().post("/session/register")
+            .then()
+            .statusCode(200)
+            .body(anything());
+    }
+}
