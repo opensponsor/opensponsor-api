931f1659ddf8f7c47164aa3b9e1a78d94b31a8a0
foxsir <838394225@qq.com>
2025-02-02 19:21:57 +0800

    feat: add property countryCode

--- src/main/java/com/opensponsor/payload/SendCodeBody.java
+++ src/main/java/com/opensponsor/payload/SendCodeBody.java
@@ -1,15 +1,19 @@
 package com.opensponsor.payload;
 
 
+import com.opensponsor.entitys.CountryCodes;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public class SendCodeBody {
+    @NotNull()
+    public CountryCodes countryCode;
+
     @Size(min = 11, max = 11)
     @NotBlank()
     @NotNull()
     @NotEmpty()
-    public String mobile;
+    public String phoneNumber;
 }
