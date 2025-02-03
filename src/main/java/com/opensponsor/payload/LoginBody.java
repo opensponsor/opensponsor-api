2d65b758925c549a5f163e65564f0f2a8c00d477
foxsir <838394225@qq.com>
2025-02-03 13:38:29 +0800

    feat: add @Schema

--- src/main/java/com/opensponsor/payload/LoginBody.java
+++ src/main/java/com/opensponsor/payload/LoginBody.java
@@ -5,17 +5,20 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
 
 public class LoginBody {
     @Size(min = 2, max = 32)
     @NotBlank()
     @NotNull()
     @NotEmpty()
+    @Schema(required = true, description = "account")
     public String account;
 
     @Size(min = 6, max = 64)
     @NotBlank()
     @NotNull()
     @NotEmpty()
+    @Schema(required = true, description = "password")
     public String password;
 }
