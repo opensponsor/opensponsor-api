c9031e073e1ad574946d35b46475a12a76aaf922
foxsir <838394225@qq.com>
2025-02-03 19:01:45 +0800

    feat: new Play Body SendSmsResponseAliyun

---
+++ src/main/java/com/opensponsor/payload/SendSmsResponseAliyun.java
@@ -0,0 +1,8 @@
+package com.opensponsor.payload;
+
+import com.aliyun.dysmsapi20170525.models.SendSmsResponse;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+
+@Schema
+public class SendSmsResponseAliyun extends SendSmsResponse {
+}
