1838a9742ca2b2c07e044feb7e51770fce78471d
foxsir <838394225@qq.com>
2025-04-06 21:09:37 +0800

    feat: add entity

---
+++ src/main/java/com/opensponsor/entitys/Tag.java
@@ -0,0 +1,39 @@
+package com.opensponsor.entitys;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.SoftDelete;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tag")
+public class Tag extends PanacheEntityBase {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(unique = true, nullable = false)
+    @Schema(required = true)
+    public UUID id;
+
+    @Column()
+    @Size(min = 2, max = 32)
+    @Schema(required = true, description = "tag name", minLength = 2, maxLength = 32)
+    public String name;
+
+    @CreationTimestamp
+    @Schema(description = "when created", required = true)
+    public Instant whenCreated;
+
+    @UpdateTimestamp
+    @Schema(description = "when modified", required = true)
+    public Instant whenModified;
+
+    @SoftDelete
+    @Column(nullable = true)
+    public Instant whenDeleted;
+}
