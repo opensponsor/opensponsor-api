7fd237c29a791f4b60349eb4333b7627b95d46e9
foxsir <838394225@qq.com>
2025-06-22 09:49:25 +0800

    add @Schema

--- src/main/java/com/opensponsor/payload/PageParams.java
+++ src/main/java/com/opensponsor/payload/PageParams.java
@@ -2,14 +2,18 @@ package com.opensponsor.payload;
 
 import io.quarkus.panache.common.Page;
 import jakarta.ws.rs.QueryParam;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
 
+@Schema
 public class PageParams {
     private static final int miniSize = 2;
     private static final int maxSize = 40;
 
+    @Schema
     @QueryParam("page")
     public int page;
 
+    @Schema
     @QueryParam("pageSize")
     public int pageSize;
 
